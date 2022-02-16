47e64a59998fd79203d8d40a45e5f3249dda0e26
al-anas <alanas7171@gmail.com>
2022-02-17 02:41:35 +0600

    income expenses js code added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
-console.log("Hello World");
+// get income amount
+function getIncome(){
+   const incomeField= document.getElementById("income-amount");
+   const incomeAmount=parseFloat(incomeField.value);
+   if(incomeAmount<0 || isNaN(incomeAmount))
+   {
+       document.getElementById("income-error").style.display="block";
+   }
+   else{
+       return incomeAmount;
+   }
+
+    
+}
+
+
+
+// get total expenses
+
+function getExpenses(){
+   
+    const foodField=document.getElementById("food-cost");
+    const foodCost=parseFloat(foodField.value);
+
+    const rentField=document.getElementById("rent-cost");
+    const rentCost=parseFloat(rentField.value);
+
+    const clothsField=document.getElementById("cloths-cost");
+    const clothsCost=parseFloat(clothsField.value);
+
+    if(foodCost<0 || rentCost<0 || clothsCost<0)
+    {
+       document.getElementById("expenses-error").style.display="block";  
+    }
+    else if(isNaN(foodCost) || isNaN(rentCost) || isNaN(clothsCost))
+    {
+       document.getElementById("expenses-error").style.display="block";    
+    }
+    else{
+        return foodCost + rentCost + clothsCost;
+    }
+    
+    
+
+}
+
+document.getElementById("calculate-btn").addEventListener("click",function(){
+    const incomeAmount=getIncome();
+    const totalCost=getExpenses();
+
+    if(totalCost>incomeAmount)
+    {
+        document.getElementById("balance-error").style.display="block"
+    }
+    else{
+        const totalBalance=incomeAmount - totalCost;
+        document.getElementById("total-expenses").innerText=totalCost;
+        document.getElementById("total-balance").innerText=totalBalance;
+    }
+
+});
